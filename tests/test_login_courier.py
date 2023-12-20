4289694cf96187d790917b5d51b53de3e53f3963
Anatoly Glinkin <tolyanbigam@gmail.com>
2023-12-20 12:05:44 +0300

    отредактировано имя фиксутры удаления курьера

--- tests/test_login_courier.py
+++ tests/test_login_courier.py
@@ -8,7 +8,7 @@ import pytest_check as check
 class TestLoginCourier:
     path = 'api/v1/courier/login'
 
-    def test_successful_login_courier(self, register_new_courier_and_return_login_password, del_data_courier):
+    def test_successful_login_courier(self, register_new_courier_and_return_login_password, fixture_del_courier):
         payload = {
             "login": register_new_courier_and_return_login_password[0],
             "password": register_new_courier_and_return_login_password[1]
@@ -21,9 +21,9 @@ class TestLoginCourier:
         assert response.status_code == 200
 
         # фикстура удаления курьера с тестовыми данными
-        del_data_courier(BASE_URL, payload['login'], payload['password'])
+        fixture_del_courier(BASE_URL, payload['login'], payload['password'])
 
-    def test_login_courier_without_password(self, register_new_courier_and_return_login_password, del_data_courier):
+    def test_login_courier_without_password(self, register_new_courier_and_return_login_password):
         payload = {
             "login": register_new_courier_and_return_login_password[0]
         }
@@ -35,7 +35,7 @@ class TestLoginCourier:
         check.equal(response.json()['message'], 'Недостаточно данных для входа')
         check.equal(response.status_code, 400)
 
-    def test_login_courier_without_login(self, register_new_courier_and_return_login_password, del_data_courier):
+    def test_login_courier_without_login(self, register_new_courier_and_return_login_password):
         payload = {
             "password": register_new_courier_and_return_login_password[1]
         }
@@ -47,7 +47,7 @@ class TestLoginCourier:
         check.equal(response.json()['message'], 'Недостаточно данных для входа')
         check.equal(response.status_code, 400)
 
-    def test_login_courier_non_existent_login(self, register_new_courier_and_return_login_password, del_data_courier):
+    def test_login_courier_non_existent_login(self, register_new_courier_and_return_login_password):
         payload = {
             "login": f'{register_new_courier_and_return_login_password[0]}_123',
             "password": register_new_courier_and_return_login_password[1]
@@ -60,7 +60,7 @@ class TestLoginCourier:
         check.equal(response.json()['message'], 'Учетная запись не найдена')
         check.equal(response.status_code, 404)
 
-    def test_login_courier_non_existent_password(self, register_new_courier_and_return_login_password, del_data_courier):
+    def test_login_courier_non_existent_password(self, register_new_courier_and_return_login_password):
         payload = {
             "login": register_new_courier_and_return_login_password[0],
             "password": f'{register_new_courier_and_return_login_password[1]}_123'
