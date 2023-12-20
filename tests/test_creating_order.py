1c0444a2cc54dd46f75b7ee1288e667e71e6f2c7
Anatoly Glinkin <tolyanbigam@gmail.com>
2023-12-20 12:02:40 +0300

    изменены тесты с подключением фикстуры

--- tests/test_creating_order.py
+++ tests/test_creating_order.py
@@ -1,37 +1,14 @@
-import json
 import pytest
-import requests
-import datetime
-from config import BASE_URL, DEFAULT_HEADERS
-import pytest_check as check
 
 
 class TestCreatingOrder:
-    path = 'api/v1/orders'
 
-    @pytest.mark.parametrize('color', ['BLACK', 'GREY', ['BLACK', 'GREY'], [""]])
-    def test_creating_order(self, color):
-        payload = {
-            "firstName": "Test",
-            "lastName": "Testov",
-            "address": "Testovaya, 142 apt.",
-            "metroStation": 4,
-            "phone": "+7 800 355 35 35",
-            "rentTime": 5,
-            "deliveryDate": datetime.date.today().strftime('%Y-%m-%d'),
-            "comment": "Test comment",
-            "color": [color]
-        }
+    @pytest.mark.parametrize('color', ['BLACK', 'GREY', ['BLACK', 'GREY'], ""])
+    def test_status_code_creating_order(self, color, fixture_creating_order):
 
-        response = requests.post(url=BASE_URL+self.path,
-                                 data=json.dumps(payload),
-                                 headers=DEFAULT_HEADERS)
+        assert fixture_creating_order(color).status_code, 202
 
-        check.equal(response.status_code, 201)
-        check.is_not(response.json()['track'], '')
+    @pytest.mark.parametrize('color', ['BLACK', 'GREY', ['BLACK', 'GREY'], ""])
+    def test_response_creating_order(self, color, fixture_creating_order):
 
-    def test_get_list_order_without_courier(self):
-        response = requests.get(url=BASE_URL+self.path)
-
-        assert len(response.json()['orders']) != 0
-
+        assert fixture_creating_order(color).json()['track'] != ''
