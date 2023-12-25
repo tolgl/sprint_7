2cba33c838a0e1ced431336acbb4346024f50e4e
Anatoly Glinkin <tolyanbigam@gmail.com>
2023-12-25 18:03:55 +0300

    навешен декторатор allure

--- tests/test_creating_order.py
+++ tests/test_creating_order.py
@@ -1,14 +1,13 @@
 import pytest
+import allure
+from conftest import fixture_creating_order
 
 
 class TestCreatingOrder:
 
+    @allure.title('Проверка ответа оформления заказа с указанием и без указания цвета')
     @pytest.mark.parametrize('color', ['BLACK', 'GREY', ['BLACK', 'GREY'], ""])
     def test_status_code_creating_order(self, color, fixture_creating_order):
 
         assert fixture_creating_order(color).status_code == 201
-
-    @pytest.mark.parametrize('color', ['BLACK', 'GREY', ['BLACK', 'GREY'], ""])
-    def test_response_creating_order(self, color, fixture_creating_order):
-
         assert fixture_creating_order(color).json()['track'] != ''
