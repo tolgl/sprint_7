d57f6aa930cc631627f46754509551b822a41041
Anatoly Glinkin <tolyanbigam@gmail.com>
2023-12-25 18:06:31 +0300

    навешен декторатор allure на тесты

--- tests/test_login_courier.py
+++ tests/test_login_courier.py
@@ -1,8 +1,7 @@
-import pytest
-
 from clients.api_client import ApiClient
 from path.path_api import ApiPath
 import allure
+from conftest import register_new_courier_and_return_login_password
 
 
 class TestLoginCourier:
@@ -10,19 +9,20 @@ class TestLoginCourier:
     @allure.title('Проверка успешного логина курьера в системе')
     def test_successful_login_courier(self, register_new_courier_and_return_login_password):
         payload = {
-            "login": register_new_courier_and_return_login_password[0],
-            "password": register_new_courier_and_return_login_password[1]
+            "login": register_new_courier_and_return_login_password[0][0],
+            "password": register_new_courier_and_return_login_password[0][1]
         }
+
         api = ApiClient()
         response = api.post(path=ApiPath.path_id_courier_login,
                             payload=payload)
-        # print(payload)
-        # print(response.json())
+
         assert response.status_code == 200
 
+    @allure.title('Проверка логина курьера без пароля')
     def test_login_courier_without_password(self, register_new_courier_and_return_login_password):
         payload = {
-            "login": register_new_courier_and_return_login_password[0]
+            "login": register_new_courier_and_return_login_password[0][0]
         }
 
         api = ApiClient()
@@ -32,9 +32,10 @@ class TestLoginCourier:
         assert response.json()['message'] == 'Недостаточно данных для входа'
         assert response.status_code == 400
 
+    @allure.title('Проверка логина курьера без логина')
     def test_login_courier_without_login(self, register_new_courier_and_return_login_password):
         payload = {
-            "password": register_new_courier_and_return_login_password[1]
+            "password": register_new_courier_and_return_login_password[0][1]
         }
 
         api = ApiClient()
@@ -44,10 +45,11 @@ class TestLoginCourier:
         assert response.json()['message'] == 'Недостаточно данных для входа'
         assert response.status_code == 400
 
+    @allure.title('Проверка логина курьера с несуществующим логином')
     def test_login_courier_non_existent_login(self, register_new_courier_and_return_login_password):
         payload = {
-            "login": f'{register_new_courier_and_return_login_password[0]}_123',
-            "password": register_new_courier_and_return_login_password[1]
+            "login": f'{register_new_courier_and_return_login_password[0][0]}_123',
+            "password": register_new_courier_and_return_login_password[0][1]
         }
 
         api = ApiClient()
@@ -57,10 +59,11 @@ class TestLoginCourier:
         assert response.json()['message'] == 'Учетная запись не найдена'
         assert response.status_code == 404
 
+    @allure.title('Проверка логина курьера с несуществующим паролем')
     def test_login_courier_non_existent_password(self, register_new_courier_and_return_login_password):
         payload = {
-            "login": register_new_courier_and_return_login_password[0],
-            "password": f'{register_new_courier_and_return_login_password[1]}_123'
+            "login": register_new_courier_and_return_login_password[0][0],
+            "password": f'{register_new_courier_and_return_login_password[0][1]}_123'
         }
 
         api = ApiClient()
