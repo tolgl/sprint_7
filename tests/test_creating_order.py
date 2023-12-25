49e6f16f972c32977c2f2aca7f00ec5de00e48b4
Anatoly Glinkin <tolyanbigam@gmail.com>
2023-12-25 18:38:45 +0300

    добавлен шаг получения номера заказа без фикстуры

--- tests/test_creating_order.py
+++ tests/test_creating_order.py
@@ -1,13 +1,18 @@
 import pytest
 import allure
-from conftest import fixture_creating_order
+from test_data import data_for_creating_order
+from clients.api_client import ApiClient
+from path.path_api import ApiPath
 
 
 class TestCreatingOrder:
 
     @allure.title('Проверка ответа оформления заказа с указанием и без указания цвета')
     @pytest.mark.parametrize('color', ['BLACK', 'GREY', ['BLACK', 'GREY'], ""])
-    def test_status_code_creating_order(self, color, fixture_creating_order):
+    def test_status_code_creating_order(self, color):
+        api = ApiClient()
+        response = api.post(path=ApiPath.path_all_orders,
+                            payload=data_for_creating_order(color))
 
-        assert fixture_creating_order(color).status_code == 201
-        assert fixture_creating_order(color).json()['track'] != ''
+        assert response.status_code == 201
+        assert response.json()['track'] != ''
