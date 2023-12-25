2e07a1540fcede958f92b334609e237af14e58fe
Anatoly Glinkin <tolyanbigam@gmail.com>
2023-12-25 18:05:42 +0300

    добавлен шаг создания курьера и навешен декторатор allure на тесты

--- tests/test_delete_courier.py
+++ tests/test_delete_courier.py
@@ -1,23 +1,55 @@
-import requests
-from config import BASE_URL
+import allure
+from test_data import generation_login_pass_name_courier
+from clients.api_client import ApiClient
+from path.path_api import ApiPath
 
 
 class TestDeleteCourier:
-    path_delete = 'api/v1/courier'
 
-    def test_successful_delete_courier(self, register_new_courier_and_return_login_password, fixture_del_courier):
+    @allure.title('Проверка успешного удаления курьера')
+    def test_successful_delete_courier(self):
         payload = {
-            "login": register_new_courier_and_return_login_password[0],
-            "password": register_new_courier_and_return_login_password[1]
+            "login": generation_login_pass_name_courier()[0],
+            "password": generation_login_pass_name_courier()[1]
         }
-        assert fixture_del_courier(url=BASE_URL, login=payload['login'], password=payload['password']).json()['ok'] is True
+        api = ApiClient()
+        # создаем курьера
+        api.post(path=ApiPath.path_creating_courier,
+                 payload=payload)
+        # получаем id курьера
+        id_courier = api.post(path=ApiPath.path_id_courier_login, payload=payload).json()["id"]
+        # удаляем курьера
+        response_del = api.delete(path=f'{ApiPath.path_creating_courier}/{id_courier}')
 
+        assert response_del.status_code == 200
+        assert response_del.json()['ok'] is True
+
+    @allure.title('Проверка удаления курьера без указания id')
     def test_delete_courier_without_id(self):
-        response_del = requests.delete(url=BASE_URL + self.path_delete)
+        payload = {
+            "login": generation_login_pass_name_courier()[0],
+            "password": generation_login_pass_name_courier()[1]
+        }
+        api = ApiClient()
+        # создаем курьера
+        api.post(path=ApiPath.path_creating_courier,
+                 payload=payload)
+        # удаляем курьера
+        response_del = api.delete(path=ApiPath.path_creating_courier)
 
         assert response_del.json()['message'] == 'Недостаточно данных для удаления курьера'
 
+    @allure.title('Проверка удаления курьера с несуществующим id')
     def test_delete_courier_non_existent_id(self):
-        response_del = requests.delete(url=f'{BASE_URL}{self.path_delete}/000000')
+        payload = {
+            "login": generation_login_pass_name_courier()[0],
+            "password": generation_login_pass_name_courier()[1]
+        }
+        api = ApiClient()
+        # создаем курьера
+        api.post(path=ApiPath.path_creating_courier,
+                 payload=payload)
+        # удаляем курьера
+        response_del = api.delete(path=f'{ApiPath.path_creating_courier}/000000')
 
         assert response_del.json()['message'] == 'Курьера с таким id нет.'
