00df450c7f572e2022db6d70ca65bd42ed3ed698
Anatoly Glinkin <tolyanbigam@gmail.com>
2023-12-25 18:06:06 +0300

    навешен декторатор allure на тесты

--- tests/test_get_order.py
+++ tests/test_get_order.py
@@ -1,15 +1,19 @@
 from clients.api_client import ApiClient
 from path.path_api import ApiPath
+import allure
+from conftest import fixture_creating_order
 
 
 class TestGetOrder:
 
-    def test_get_list_order_without_courier(self):
+    @allure.title('Проверка получения полного списка заказа')
+    def test_get_full_list_order(self):
         api = ApiClient()
         response = api.get(path=ApiPath.path_all_orders)
 
         assert len(response.json()['orders']) != 0
 
+    @allure.title('Проверка получения заказа по номеру')
     def test_get_order_by_track(self, fixture_creating_order):
         get_params = {"t": fixture_creating_order(color='BLACK').json()['track']}
         api = ApiClient()
@@ -17,12 +21,14 @@ class TestGetOrder:
 
         assert response.json()['order']['track'] == get_params['t']
 
+    @allure.title('Проверка получения заказа без указания номеру')
     def test_get_order_without_track(self):
         api = ApiClient()
         response = api.get(path=ApiPath.path_order_by_track)
 
         assert response.json()['message'] == 'Недостаточно данных для поиска'
 
+    @allure.title('Проверка получения заказа по несуществующему номеру')
     def test_get_order_non_existent_track(self):
         get_params = {"t": 000000}
         api = ApiClient()
